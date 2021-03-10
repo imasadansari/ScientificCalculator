46a6c5abe4861e5bb87830eca9bbe9b12047b2db
imasadansari <mohammadasad.ansari@iiitb.org>
2021-03-10 18:34:45 +0530

    All buttons Made in Gui of both standard and scientific part no functionality attached along with them till now

--- SciCalc.py
+++ SciCalc.py
@@ -67,7 +67,85 @@ btnPM = Button(calc, text= chr(177), width = 6, height = 2, font=('ariel', 20, '
 # Evaluate or equals
 btnEquals = Button(calc, text= "=", width = 6, height = 2, font=('ariel', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 5, column = 3, pady = 1)
 
+#-------------------------------- Scientific Calci----------------------------
 
+# ======================     Row 1	======================================
+# Pi - π
+btnPi = Button(calc, text= "π", width = 6, height = 2, font=('ariel', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 1, column = 4, pady = 1)
+
+# Cos 
+btnSq = Button(calc, text= "cos", width = 6, height = 2, font=('ariel', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 1, column = 5, pady = 1)
+
+# tan
+btnAdd = Button(calc, text= "tan", width = 6, height = 2, font=('ariel', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 1, column = 6, pady = 1)
+
+
+# Sin
+btnSub = Button(calc, text= "sin", width = 6, height = 2, font=('ariel', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 1, column = 7, pady = 1)
+
+# =======================	Row 2	===============================
+# 2 pi
+btn2Pi = Button(calc, text= "2π", width = 6, height = 2, font=('ariel', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 2, column = 4, pady = 1)
+
+# Cosh
+btnCosh = Button(calc, text= "cosh", width = 6, height = 2, font=('ariel', 20, 'bold'), bd = 4).grid(row = 2, column = 5, pady = 1)
+
+# Tamh
+btnTanh = Button(calc, text= "tanh", width = 6, height = 2, font=('ariel', 20, 'bold'), bd = 4).grid(row = 2, column = 6, pady = 1)
+
+
+# Sinh
+btnSinh = Button(calc, text= "sinh", width = 6, height = 2, font=('ariel', 20, 'bold'), bd = 4).grid(row = 2, column = 7, pady = 1)
+
+
+
+# =======================	Row 3	===============================
+
+# 2 log
+btnLog = Button(calc, text= "log", width = 6, height = 2, font=('ariel', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 3, column = 4, pady = 1)
+
+# exponential Exp
+btnExp = Button(calc, text= "Exp", width = 6, height = 2, font=('ariel', 20, 'bold'), bd = 4).grid(row = 3, column = 5, pady = 1)
+
+# Mod
+btnMod = Button(calc, text= "Mod", width = 6, height = 2, font=('ariel', 20, 'bold'), bd = 4).grid(row = 3, column = 6, pady = 1)
+
+# E - e
+btnE = Button(calc, text= "e", width = 6, height = 2, font=('ariel', 20, 'bold'), bd = 4).grid(row = 3, column = 7, pady = 1)
+
+
+# =======================	Row 4	===============================
+# log2
+btnLog2 = Button(calc, text= "log2", width = 6, height = 2, font=('ariel', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 4, column = 4, pady = 1)
+
+# deg
+btnDeg = Button(calc, text= "deg", width = 6, height = 2, font=('ariel', 20, 'bold'), bd = 4).grid(row = 4, column = 5, pady = 1)
+
+# Cosh
+btnCosh = Button(calc, text= "acosh", width = 6, height = 2, font=('ariel', 20, 'bold'), bd = 4).grid(row = 4, column = 6, pady = 1)
+
+
+# asinh
+btnASinh = Button(calc, text= "asinh", width = 6, height = 2, font=('ariel', 20, 'bold'), bd = 4).grid(row = 4, column = 7, pady = 1)
+
+
+# =======================	Row 5	===============================
+# log base 10
+btnLog10 = Button(calc, text= "log10", width = 6, height = 2, font=('ariel', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 5, column = 4, pady = 1)
+
+# Log1p
+btnLog1p = Button(calc, text= "log1p", width = 6, height = 2, font=('ariel', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 5, column = 5, pady = 1)
+
+# Expm1
+btnExpm1 = Button(calc, text= "expm1", width = 6, height = 2, font=('ariel', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 5, column = 6, pady = 1)
+
+
+# gamma
+btnLgamma = Button(calc, text= "lgamma", width = 6, height = 2, font=('ariel', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 5, column = 7, pady = 1)
+
+# label display extend for scientific
+lblDisplay = Label(calc, text="Scientific Calculator", font=('ariel', 30, 'bold'), justify = CENTER)
+lblDisplay.grid(row = 0, column = 4, columnspan = 4)
 
 
 # ---------------------Menu & Functions--------------------------------------
@@ -81,7 +159,7 @@ def iExit():
 
 def Scientific():
 	root.resizable(width=False, height=False)
-	root.geometry("960x568+0+0")
+	root.geometry("1200x568+0+0")
 	
 	
 def Standard():
